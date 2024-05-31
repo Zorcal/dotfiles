53edc1f8f17497936ab2105d1780f8ba851f3b66
Zorcal <zorcal.dev@gmail.com>
2024-05-31 19:19:10 +0200

    nvim: update terminal size

--- nvim/.config/nvim/lua/zorcal/plugins/terminal.lua
+++ nvim/.config/nvim/lua/zorcal/plugins/terminal.lua
@@ -5,7 +5,7 @@ return {
     require("toggleterm").setup {
       size = function(term)
         if term.direction == "horizontal" then
-          return 20
+          return 40
         elseif term.direction == "vertical" then
           return vim.o.columns * 0.4
         end
